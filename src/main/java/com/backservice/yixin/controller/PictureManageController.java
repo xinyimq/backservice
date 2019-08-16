00779e0d38f695154b1652f53386fe1495f731e3
viruser <wmwyang@163.com>
2019-08-16 10:54:08 +0800

    图片管理

---
+++ src/main/java/com/backservice/yixin/controller/PictureManageController.java
@@ -0,0 +1,13 @@
+package com.backservice.yixin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 照片管理controller
+ */
+
+@RestController("pictureManageController")
+@RequestMapping("/picture")
+public class PictureManageController {
+}
